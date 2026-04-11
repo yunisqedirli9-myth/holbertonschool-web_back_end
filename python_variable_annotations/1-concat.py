872762d28b4fcad375a90a05728346c9db4cc3b9
yunisqedirli9-myth <yunisqedirli9@gmail.com>
2026-04-11 16:54:18 +0400

    Update 1-concat.py

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,1 +1,7 @@
+#!/usr/bin/env python3
+"""Module that provides a concatenation function."""
 
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings and returns the result."""
+    return str1 + str2
