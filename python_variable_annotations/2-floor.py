49376358eabe06a8199c1f0ca1d0a4af0be436ef
yunisqedirli9-myth <yunisqedirli9@gmail.com>
2026-04-11 16:54:42 +0400

    Implement floor function using math.floor
    Adds a floor function that returns the largest integer less than or equal to the given float.

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that provides a floor function."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of the float n."""
+    return math.floor(n)
