3619e14c030c42f28c4b46291d92b1a825d7d499
yunisqedirli9-myth <yunisqedirli9@gmail.com>
2026-05-02 23:30:49 +0400

    Create 2-hypermedia_pagination.py

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""Hypermedia pagination module."""
+
+import csv
+import math
+from typing import Dict, List
+
+
+def index_range(page, page_size):
+    """Return a tuple containing the start and end indexes for pagination."""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset."""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return the requested page of the dataset."""
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+
+        start_index, end_index = index_range(page, page_size)
+        data = self.dataset()
+
+        if start_index >= len(data):
+            return []
+
+        return data[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Return hypermedia pagination metadata for a requested page."""
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages,
+        }
