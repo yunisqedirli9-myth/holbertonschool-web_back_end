f518e5e3283bf91c039c3e55858fd8b4aacf6ab3
yunisqedirli9-myth <yunisqedirli9@gmail.com>
2026-05-09 22:58:03 +0400

    Create 9-insert_school.py

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module for inserting a document into a MongoDB collection."""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection based on kwargs.
+    Returns the new _id.
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
