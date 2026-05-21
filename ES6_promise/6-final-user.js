e4c1a53b9ea4076e9d01ee243254a4c561cd5c0c
yunisqedirli9-myth <yunisqedirli9@gmail.com>
2026-05-22 03:23:02 +0400

    Update 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,17 @@
-// Handle multiple promises
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
-function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  const results = await Promise.allSettled([userPromise, photoPromise]);
+
+  return results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return { status: result.status, value: result.value };
+    }
+    // Rejected olanların dəyərini sadəcə String(Error) olaraq value içinə qoyuruq
+    return { status: result.status, value: String(result.reason) };
+  });
 }
-
-export default handleProfileSignup;
