48baaa939829a28cef6c8c133d15d0ee0c29331d
yunisqedirli9-myth <yunisqedirli9@gmail.com>
2026-06-02 23:23:19 +0400

    Add displayMessage function to print messages

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Prints a message to the standard output (STDOUT).
+ * @param {string} message - The string to be printed.
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
