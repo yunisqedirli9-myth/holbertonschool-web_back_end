3ff6a087b277eb62ef9ceafe79b7185e6bc1a3d4
yunisqedirli9-myth <yunisqedirli9@gmail.com>
2026-06-02 23:25:40 +0400

    Create a basic HTTP server in 4-http.js

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const PORT = 1245;
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+
+app.listen(PORT);
+
+module.exports = app;
